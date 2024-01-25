d350401a84c92b8335315121b2a3ccd39dad76ab
bianca9901 <biancawikberg99@yahoo.com>
2024-01-25 10:08:19 +0100

    Add pages and setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import {Route, Routes} from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+
 function App() {
 
   return (
-<h1>Hej</h1>
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/store" element={<Store/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+      </Routes>
+    </Container>
+
+
   )
 }
 
