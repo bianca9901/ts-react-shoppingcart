537c9e68af247b0bbddb517b7eda7d6153635582
bianca9901 <biancawikberg99@yahoo.com>
2024-01-25 16:26:50 +0100

    Add shopping-cart-provider wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import {Route, Routes} from "react-router-dom"
-import { Container } from "react-bootstrap"
-import { Home } from "./pages/Home"
-import { Store } from "./pages/Store"
-import { About } from "./pages/About"
-import { NavBar } from "./components/NavBar"
+import { Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
+import { NavBar } from "./components/NavBar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 function App() {
-
   return (
-    <>
-    <NavBar />
-    <Container className="mb-4">
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/store" element={<Store/>}></Route>
-        <Route path="/about" element={<About/>}></Route>
-      </Routes>
-    </Container>
-    </>
-
-  )
+    <ShoppingCartProvider>
+      <NavBar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/store" element={<Store />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
+  );
 }
 
-export default App
+export default App;
